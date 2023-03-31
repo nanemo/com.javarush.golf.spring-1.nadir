670a355bde2b1d83f5194fee3d6d0ef1c2bfb1b7
Nadir Nadirli <companynadirnadirli@gmail.com>
2023-04-01 03:59:26 +0400

    I worked on controllers. All methods are ready.

--- repository/src/main/java/com/javarush/controller/TaskController.java
+++ repository/src/main/java/com/javarush/controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.javarush.controller;
 
+import com.javarush.entity.Task;
 import com.javarush.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -16,20 +19,32 @@ public class TaskController {
 
     @GetMapping("/")
     public String tasks(Model model,
-                            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
-                            @RequestParam(value = "limit", required = false, defaultValue = "10") int limit){
+                        @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                        @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
+        List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
+        model.addAttribute("tasks", tasks);
         return "tasks";
     }
 
     @PostMapping("/{id}")
     public void edit(Model model,
-                     @PathVariable Integer id,
-                     @RequestBody TaskInfo taskInfo){
+                     @PathVariable(name = "id") String sID,
+                     @RequestBody TaskInfo taskInfo) {
+        Task editedTask = taskService.edit(sID, taskInfo.getDescription(), taskInfo.getStatus());
+        model.addAttribute("tasks", editedTask);
     }
 
+    @PostMapping("/")
+    public void add(Model model,
+                    @RequestBody TaskInfo taskInfo) {
+        Task newTask = taskService.create(taskInfo.getDescription(), taskInfo.getStatus());
+        model.addAttribute("tasks", newTask);
+    }
 
-
-
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(name = "id") String sID) {
+        taskService.delete(sID);
+    }
 
     public TaskService getTaskService() {
         return taskService;
